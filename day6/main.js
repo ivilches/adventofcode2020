543e730607b5a9c591705608d9b2c62527c5b11e
PLAINCONCEPTS\ivilches <Israel Vilches>
2020-12-06 14:04:05 +0100

    Day 6 - second star

--- day6/main.js
+++ day6/main.js
@@ -1,25 +1,47 @@
-let letters = [];
-let grouped = [];
+let groupAnswers = [];
+let firstExerciseResult = 0;
+let secondExerciseResult = 0;
+let peopleInGroup = 0;
 
-const processAnswer = (answer) => {
+const processAnswer_First = (answer) => {
     if (answer !== '') {
-        letters = [...letters, ...answer.split('')];
+        groupAnswers = [...groupAnswers, ...answer.split('')];
     }
     else {
         const uniques = new Set();
-        letters.forEach(l => uniques.add(l));
-        letters = [];
-        grouped.push(uniques.size);
+        groupAnswers.forEach(l => uniques.add(l));
+        firstExerciseResult += uniques.size;
+
+        groupAnswers = [];
     }
 }
+
+const processAnswer_Second = (answer) => {
+    if (answer !== '') {
+        groupAnswers = [...groupAnswers, ...answer.split('')];
+        peopleInGroup += 1;
+    }
+    else {
+        const counted = groupAnswers.reduce((acum,cur) => Object.assign(acum, { [cur]: (acum[cur] || 0) + 1 }), {});
+        const answeredGroupally = groupAnswers.filter(l => (counted[l] >= peopleInGroup));
+        const uniques = new Set();
+        answeredGroupally.forEach(a => uniques.add(a));
+        secondExerciseResult += uniques.size;
+
+        groupAnswers = [];
+        peopleInGroup = 0;
+    }
+}
+
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('input.txt')
 });
 
 
-lineReader.on('line', (line) => processAnswer(line));
+lineReader.on('line', (line) => processAnswer_First(line));
 
 lineReader.on('close', () => {
-    console.log(grouped.reduce((a, b) => a + b));
+    console.log(`First exercise answer: ${firstExerciseResult}`);
+    // console.log(`Second exercise answer ${secondExerciseAnswer}`);
 });
